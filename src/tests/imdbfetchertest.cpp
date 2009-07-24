a77b16b3a0d9ba07dd89450b3ca7ae38e56de83d
Robby Stephenson <robby@periapsis.org>
2009-07-24 16:11:20 +0000

    add initial imdb fetch test
    svn path=/trunk/extragear/office/tellico/; revision=1001962

---
+++ src/tests/imdbfetchertest.cpp
@@ -0,0 +1,77 @@
+/***************************************************************************
+    Copyright (C) 2009 Robby Stephenson <robby@periapsis.org>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU General Public License as        *
+ *   published by the Free Software Foundation; either version 2 of        *
+ *   the License or (at your option) version 3 or any later version        *
+ *   accepted by the membership of KDE e.V. (or its successor approved     *
+ *   by the membership of KDE e.V.), which shall act as a proxy            *
+ *   defined in Section 14 of version 3 of the license.                    *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ ***************************************************************************/
+
+#undef QT_NO_CAST_FROM_ASCII
+
+#include "qtest_kde.h"
+#include "imdbfetchertest.h"
+#include "imdbfetchertest.moc"
+
+#include "../fetch/fetcherjob.h"
+#include "../fetch/imdbfetcher.h"
+#include "../entry.h"
+#include "../collections/videocollection.h"
+#include "../collectionfactory.h"
+#include "../images/imagefactory.h"
+
+QTEST_KDEMAIN_CORE( ImdbFetcherTest )
+
+ImdbFetcherTest::ImdbFetcherTest() : m_loop(this) {
+}
+
+void ImdbFetcherTest::initTestCase() {
+  Tellico::ImageFactory::init();
+  Tellico::RegisterCollection<Tellico::Data::VideoCollection> registerVideo(Tellico::Data::Collection::Video, "video");
+}
+
+void ImdbFetcherTest::testSnowyRiver() {
+  Tellico::Fetch::FetchRequest request(Tellico::Data::Collection::Video, Tellico::Fetch::Title, "The Man From Snowy River");
+  Tellico::Fetch::Fetcher::Ptr fetcher(new Tellico::Fetch::IMDBFetcher(this));
+
+  // don't use 'this' as job parent, it crashes
+  Tellico::Fetch::FetcherJob* job = new Tellico::Fetch::FetcherJob(0, fetcher, request);
+  job->setMaximumResults(1);
+  connect(job, SIGNAL(result(KJob*)), this, SLOT(slotResult(KJob*)));
+
+  job->start();
+  m_loop.exec();
+
+  QCOMPARE(m_results.size(), 1);
+
+  // the first entry had better be the right onw
+  Tellico::Data::EntryPtr entry = m_results.at(0);
+
+  QCOMPARE(entry->field("title"), QLatin1String("The Man from Snowy River"));
+  QCOMPARE(entry->field("year"), QLatin1String("1982"));
+  QCOMPARE(entry->field("genre"), QLatin1String("Drama; Family; Romance; Western"));
+  QCOMPARE(entry->field("nationality"), QLatin1String("Australia"));
+  QCOMPARE(entry->field("studio"), QLatin1String("Cambridge Productions"));
+  QCOMPARE(entry->field("running-time"), QLatin1String("102"));
+  QCOMPARE(entry->fields("cast", false).at(0), QLatin1String("Tom Burlinson::Jim Craig"));
+}
+
+void ImdbFetcherTest::slotResult(KJob* job_) {
+  m_results = static_cast<Tellico::Fetch::FetcherJob*>(job_)->entries();
+  m_loop.quit();
+}
