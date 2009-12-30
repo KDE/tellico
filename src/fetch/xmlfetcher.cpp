f2de056e47f73da06cec18042284e029768f1b89
Robby Stephenson <robby@periapsis.org>
2009-12-30 00:59:27 +0000

    work in progress to add class for generic xml fetching
    svn path=/trunk/extragear/office/tellico/; revision=1067614

---
+++ src/fetch/xmlfetcher.cpp
@@ -0,0 +1,208 @@
+/***************************************************************************
+    Copyright (C) 2008-2009 Robby Stephenson <robby@periapsis.org>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU General Public License as        *
+ *   published by the Free Software Foundation; either version 2 of        *
+ *   the License or (at your option) version 3 or any later version        *
+ *   accepted by the membership of KDE e.V. (or its successor approved     *
+ *   by the membership of KDE e.V.), which shall act as a proxy            *
+ *   defined in Section 14 of version 3 of the license.                    *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "xmlfetcher.h"
+#include "../translators/xslthandler.h"
+#include "../translators/tellicoimporter.h"
+#include "../gui/guiproxy.h"
+#include "../tellico_utils.h"
+#include "../collection.h"
+#include "../entry.h"
+#include "../tellico_debug.h"
+
+#include <klocale.h>
+#include <kstandarddirs.h>
+#include <kio/job.h>
+#include <kio/jobuidelegate.h>
+
+#include <QFile>
+#include <QTextStream>
+#include <QTextCodec>
+
+using Tellico::Fetch::XMLFetcher;
+
+XMLFetcher::XMLFetcher(QObject* parent_) : Fetcher(parent_)
+    , m_xsltHandler(0)
+    , m_started(false)
+    , m_limit(0) {
+}
+
+XMLFetcher::~XMLFetcher() {
+  delete m_xsltHandler;
+  m_xsltHandler = 0;
+}
+
+void XMLFetcher::search() {
+  m_started = true;
+  resetSearch();
+  doSearch();
+}
+
+void XMLFetcher::continueSearch() {
+  m_started = true;
+  doSearch();
+}
+
+void XMLFetcher::doSearch() {
+  KUrl u = searchUrl();
+  Q_ASSERT(!u.isEmpty());
+  if(u.isEmpty()) {
+    stop();
+    return;
+  }
+//  myDebug() << "url: " << u.url();
+
+  m_job = KIO::storedGet(u, KIO::NoReload, KIO::HideProgressInfo);
+  m_job->ui()->setWindow(GUI::Proxy::widget());
+  connect(m_job, SIGNAL(result(KJob*)), SLOT(slotComplete(KJob*)));
+}
+
+void XMLFetcher::stop() {
+  if(!m_started) {
+    return;
+  }
+  if(m_job) {
+    m_job->kill();
+    m_job = 0;
+  }
+  m_started = false;
+  emit signalDone(this);
+}
+
+void XMLFetcher::slotComplete(KJob* ) {
+//  myDebug();
+  if(m_job->error()) {
+    m_job->ui()->showErrorMessage();
+    stop();
+    return;
+  }
+
+  QByteArray data = m_job->data();
+  if(data.isEmpty()) {
+    myDebug() << "no data";
+    stop();
+    return;
+  }
+
+#if 0
+  myWarning() << "Remove debug from XMLFetcher.cpp";
+  QFile f(QLatin1String("/tmp/test.xml"));
+  if(f.open(QIODevice::WriteOnly)) {
+    QTextStream t(&f);
+    t.setEncoding(QTextStream::UnicodeUTF8);
+    t << data;
+  }
+  f.close();
+#endif
+
+  if(!m_xsltHandler) {
+    initXSLTHandler();
+    if(!m_xsltHandler) { // probably an error somewhere in the stylesheet loading
+      stop();
+      return;
+    }
+  }
+
+  parseData(data);
+
+  // assume data is always utf-8
+  QString str = m_xsltHandler->applyStylesheet(QString::fromUtf8(data, data.size()));
+  Import::TellicoImporter imp(str);
+  Data::CollPtr coll = imp.collection();
+  if(!coll) {
+    myDebug() << "no collection pointer";
+    stop();
+    return;
+  }
+
+  int count = 0;
+  foreach(Data::EntryPtr entry, coll->entries()) {
+    if(count >= m_limit) {
+      break;
+    }
+    if(!m_started) {
+      // might get aborted
+      break;
+    }
+
+    FetchResult* r = new FetchResult(Fetcher::Ptr(this), entry);
+    m_entries.insert(r->uid, entry);
+    emit signalResultFound(r);
+    ++count;
+  }
+
+  stop(); // required
+}
+
+Tellico::Data::EntryPtr XMLFetcher::fetchEntryHookHook(uint uid_) {
+  Data::EntryPtr entry = m_entries[uid_];
+  if(!entry) {
+    myWarning() << "no entry in dict";
+    return Data::EntryPtr();
+  }
+
+  // the fetcher might end up creating a new entry
+  return fetchEntryHookData(entry);
+}
+
+void XMLFetcher::initXSLTHandler() {
+  Q_ASSERT(!m_xsltFilename.isEmpty());
+  QString xsltfile = KStandardDirs::locate("appdata", m_xsltFilename);
+  if(xsltfile.isEmpty()) {
+    myWarning() << "can not locate" << m_xsltFilename;
+    return;
+  }
+
+  KUrl u;
+  u.setPath(xsltfile);
+
+  delete m_xsltHandler;
+  m_xsltHandler = new XSLTHandler(u);
+  if(!m_xsltHandler->isValid()) {
+    myWarning() << "error in" << m_xsltFilename;
+    delete m_xsltHandler;
+    m_xsltHandler = 0;
+    return;
+  }
+}
+
+void XMLFetcher::setXSLTFilename(const QString& filename_) {
+  if(!filename_.isEmpty() && filename_ != m_xsltFilename) {
+    m_xsltFilename = filename_;
+    delete m_xsltHandler;
+    m_xsltHandler = 0;
+  }
+}
+
+void XMLFetcher::setLimit(int limit_) {
+  Q_ASSERT(m_limit > 0);
+  m_limit = limit_;
+}
+
+Tellico::XSLTHandler* XMLFetcher::xsltHandler() {
+  Q_ASSERT(m_xsltHandler);
+  return m_xsltHandler;
+}
+
+#include "xmlfetcher.moc"
---
+++ src/fetch/xmlfetcher.h
@@ -0,0 +1,95 @@
+/***************************************************************************
+    Copyright (C) 2008-2009 Robby Stephenson <robby@periapsis.org>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU General Public License as        *
+ *   published by the Free Software Foundation; either version 2 of        *
+ *   the License or (at your option) version 3 or any later version        *
+ *   accepted by the membership of KDE e.V. (or its successor approved     *
+ *   by the membership of KDE e.V.), which shall act as a proxy            *
+ *   defined in Section 14 of version 3 of the license.                    *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef TELLICO_XMLFETCHER_H
+#define TELLICO_XMLFETCHER_H
+
+#include "fetcher.h"
+#include "../datavectors.h"
+
+#include <QPointer>
+#include <QHash>
+
+class KUrl;
+class KJob;
+namespace KIO {
+  class StoredTransferJob;
+}
+
+namespace Tellico {
+
+  class XSLTHandler;
+
+  namespace Fetch {
+
+/**
+ * @author Robby Stephenson
+ */
+class XMLFetcher : public Fetcher {
+Q_OBJECT
+
+public:
+  /**
+   */
+  XMLFetcher(QObject* parent);
+  /**
+   */
+  virtual ~XMLFetcher();
+
+  virtual bool isSearching() const { return m_started; }
+  virtual void continueSearch();
+  virtual void stop();
+  virtual Data::EntryPtr fetchEntryHookHook(uint uid);
+
+protected:
+  void setXSLTFilename(const QString& filename);
+  void setLimit(int limit);
+  XSLTHandler* xsltHandler();
+
+private slots:
+  void slotComplete(KJob* job);
+
+private:
+  virtual void search();
+  virtual void resetSearch() = 0;
+  virtual KUrl searchUrl() = 0;
+  virtual void parseData(const QByteArray& data) = 0;
+  virtual Data::EntryPtr fetchEntryHookData(Data::EntryPtr entry) = 0;
+
+  void initXSLTHandler();
+  void doSearch();
+
+  QString m_xsltFilename;
+  XSLTHandler* m_xsltHandler;
+
+  QPointer<KIO::StoredTransferJob> m_job;
+  QHash<int, Data::EntryPtr> m_entries;
+
+  bool m_started;
+  int m_limit;
+};
+
+  } // end namespace
+} // end namespace
+#endif
