45de2ef175fffe8b84d68648a65bea2532ecc7d2
Robby Stephenson <robby@periapsis.org>
2010-03-25 03:37:37 +0000

    Strip control characters (0x00 to 0xFF) from text files on import.
    BUG:231302
    
    svn path=/trunk/extragear/office/tellico/; revision=1107232

--- src/translators/textimporter.cpp
+++ src/translators/textimporter.cpp
@@ -25,12 +25,23 @@
 #include "textimporter.h"
 #include "../core/filehandler.h"
 
+#include <QRegExp>
+
+namespace {
+  QString& cleanXml(QString s) {
+    // remove C0 Control Characters, since we assume we're importing a file
+    // with contents that will be represented  in XML later...
+    static const QRegExp rx(QLatin1String("[\x0000-\x00FF]"));
+    return s.remove(rx);
+  }
+}
+
 using Tellico::Import::TextImporter;
 
 TextImporter::TextImporter(const KUrl& url_, bool useUTF8_)
     : Import::Importer(url_) {
   if(url_.isValid()) {
-    setText(FileHandler::readTextFile(url_, false, useUTF8_));
+    setText(cleanXml(FileHandler::readTextFile(url_, false, useUTF8_)));
   }
 }
 
