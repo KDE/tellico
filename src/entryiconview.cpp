3a9847541c581e8274604eaa7bd6aa1ab19e9c2d
Robby Stephenson <robby@periapsis.org>
2010-09-12 18:51:37 +0000

    Increase max entry icon size to 256.
    CCBUG:250907
    
    svn path=/trunk/extragear/office/tellico/; revision=1174530

--- src/entryiconview.cpp
+++ src/entryiconview.cpp
@@ -45,7 +45,7 @@
 
 namespace {
   static const int MIN_ENTRY_ICON_SIZE = 32;
-  static const int MAX_ENTRY_ICON_SIZE = 128;
+  static const int MAX_ENTRY_ICON_SIZE = 256;
   static const int ENTRY_ICON_SIZE_PAD = 6;
   static const int ENTRY_ICON_SHADOW_RIGHT = 1;
   static const int ENTRY_ICON_SHADOW_BOTTOM = 1;
