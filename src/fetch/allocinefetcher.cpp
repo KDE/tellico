efea772ba2881d34e7330e54d6937004716f8607
Robby Stephenson <robby@periapsis.org>
2014-05-25 17:01:59 -0700

    Fix character encoding in Allocine fetcher for commas and apostrophes
    Removing the commas and replacing apostrophes with spaces seems to
    provide appropriate results. Go ahead and remove accents marks, too,
    since that also doesn't seem to matter
    
    BUG: 334527
    FIXED-IN: 2.3.9

--- src/fetch/allocinefetcher.cpp
+++ src/fetch/allocinefetcher.cpp
@@ -103,8 +103,13 @@ void AbstractAllocineFetcher::search() {
   QList<QPair<QString, QString> > params;
   params.append(qMakePair(QString::fromLatin1("partner"), m_apiKey));
 
-  QString q = request().value;
+  // I can't figure out how to encode accent marks, but they don't
+  // seem to be necessary
+  QString q = removeAccents(request().value);
+  q.remove(QLatin1Char(','));
+  q.replace(QLatin1Char('\''), QLatin1Char('+'));
   q.replace(QLatin1Char(' '), QLatin1Char('+'));
+
   switch(request().key) {
     case Keyword:
       params.append(qMakePair(QString::fromLatin1("q"), q));
