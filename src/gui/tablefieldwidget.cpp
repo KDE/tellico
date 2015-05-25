4e00b38df9b08c28090a485c092c5dabf32af5c5
Robby Stephenson <robby@periapsis.org>
2015-05-24 19:06:22 -0700

    Don't hide the horizontal scrollbar in the table widget
    Tables with many columns could easily expand past the dialog widget.
    Don't disable the horizontal scrollbar.
    
    BUG: 348189
    FIXED-IN: 2.3.11

--- src/gui/tablefieldwidget.cpp
+++ src/gui/tablefieldwidget.cpp
@@ -65,7 +65,7 @@ TableFieldWidget::TableFieldWidget(Tellico::Data::FieldPtr field_, QWidget* pare
   m_table->horizontalHeader()->setResizeMode(m_columns-1, QHeaderView::Interactive);
   m_table->resizeColumnToContents(m_columns-1);
   m_table->setSelectionMode(QAbstractItemView::NoSelection);
-  m_table->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+//  m_table->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
   // capture all the context menu events
   m_table->setContextMenuPolicy(Qt::CustomContextMenu);
