819c7ab37c188682c6e09f2caf72dc4264e9dc91
Robby Stephenson <robby@periapsis.org>
2015-12-13 22:38:09 -0500

    Proper variable initialization order

--- src/fetch/multifetcher.cpp
+++ src/fetch/multifetcher.cpp
@@ -40,7 +40,7 @@ using namespace Tellico;
 using Tellico::Fetch::MultiFetcher;
 
 MultiFetcher::MultiFetcher(QObject* parent_)
-    : Fetcher(parent_), m_started(false), m_collType(0) {
+    : Fetcher(parent_), m_collType(0), m_started(false) {
 }
 
 MultiFetcher::~MultiFetcher() {
