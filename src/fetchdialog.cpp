2eb8dfb28474bd556f89ec5be87ba0d5ea6bd527
Robby Stephenson <robby@periapsis.org>
2016-01-22 15:35:17 -0500

    Clear search results for each new search
    BUG: 357799
    REVIEW: 126705

--- src/fetchdialog.cpp
+++ src/fetchdialog.cpp
@@ -371,6 +371,7 @@ void FetchDialog::slotSearchClicked() {
     Fetch::Manager::self()->stop();
     slotFetchDone();
   } else {
+    m_treeWidget->clear();
     const QString value = m_valueLineEdit->text().simplified();
     m_resultCount = 0;
     m_oldSearch = value;
