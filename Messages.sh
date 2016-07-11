faab9e482571adcae9a0901e811fdc2ee1d7d8f7
Pino Toscano <pino@kde.org>
2016-07-11 07:41:40 +0200

    prepare_i18n_xslt: output proper source references
    Output a comment with the references to the source of each extracted
    string: this way we can append the output of prepare_i18n_xslt to
    rc.cpp, and repack-pot will change the source references from rc.cpp
    to the real ones.

--- Messages.sh
+++ Messages.sh
@@ -4,10 +4,9 @@ $EXTRACTRC `find . -name '*.rc' -o -name '*.ui' -o -name '*.kcfg'` >> rc.cpp
 # extract tips
 ./preparetips tellico.tips > tips.cpp
 # extract other translation files
-./prepare_i18n_xslt > xslt.cpp
+./prepare_i18n_xslt >> rc.cpp
 # extract messages
 $XGETTEXT `find . -name '*.cpp' -o -name '*.h' -o -name '*.c'` -o $podir/tellico.pot
 # cleanup
 rm -f rc.cpp
 rm -f tips.cpp
-rm -f xslt.cpp
