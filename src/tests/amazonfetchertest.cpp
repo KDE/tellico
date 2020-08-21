d66900fe431ef3ac404b401a4670da7c485c843e
Robby Stephenson <robby@periapsis.org>
2020-08-21 17:32:50 -0400

    Combine client and error test for data source

--- src/tests/amazonfetchertest.cpp
+++ src/tests/amazonfetchertest.cpp
@@ -442,29 +442,6 @@ void AmazonFetcherTest::testPayload() {
   QCOMPARE(payload, res1);
 }
 
-void AmazonFetcherTest::testClient() {
-  KSharedConfig::Ptr config = KSharedConfig::openConfig(QString(), KConfig::SimpleConfig);
-  KConfigGroup cg(config, QStringLiteral("AmazonClient"));
-  cg.writeEntry("Site", 0); // US
-  cg.writeEntry("Image Size", 3); // no image
-  cg.writeEntry("AccessKey", QStringLiteral("AKIAIYOQSCRWEGQPCSNA"));
-  cg.writeEntry("SecretKey", QStringLiteral("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"));
-  cg.writeEntry("AssocToken", QStringLiteral("tellico-20"));
-  config->sync();
-
-  Tellico::Fetch::AmazonFetcher* fetcher = new Tellico::Fetch::AmazonFetcher(this);
-  fetcher->readConfig(cg, cg.name());
-
-  Tellico::Fetch::FetchRequest req(Tellico::Data::Collection::Book, Tellico::Fetch::Keyword, "harry potter");
-  Tellico::Fetch::Fetcher::Ptr f(fetcher);
-  Tellico::Fetch::MessageLogger* logger = new Tellico::Fetch::MessageLogger;
-  f->setMessageHandler(logger);
-  Tellico::Data::EntryList results = DO_FETCH1(f, req, 1);
-  QVERIFY(results.isEmpty());
-  QVERIFY(!logger->errorList.isEmpty());
-  QVERIFY(logger->errorList[0].startsWith("The Access Key Id AKIAIYOQSCRWEGQPCSNA is not enabled for accessing Product Advertising API"));
-}
-
 void AmazonFetcherTest::testError() {
   Tellico::Fetch::FetchRequest request(Tellico::Data::Collection::Book, Tellico::Fetch::UPC, "717356278525");
   Tellico::Fetch::AmazonFetcher* f = new Tellico::Fetch::AmazonFetcher(this);
@@ -475,7 +452,6 @@ void AmazonFetcherTest::testError() {
   f->m_site = Tellico::Fetch::AmazonFetcher::US;
   f->m_accessKey = QStringLiteral("test");
   f->m_secretKey = QStringLiteral("test");
-  f->m_testResultsFile = QFINDTESTDATA("data/amazon-paapi-error1.json");
 
   Tellico::Data::EntryList results = DO_FETCH1(fetcher, request, 1);
   QVERIFY(!logger->errorList.isEmpty());
