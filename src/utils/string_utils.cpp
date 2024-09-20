ee9283c7700e87bc10beff2e0458449e0fd636f3
Robby Stephenson <robby@periapsis.org>
2024-09-20 18:05:28 -0400

    Use "UTF-8" instead of "Locale" for system encoding name
    In Qt6, QStringConverter::System is named "Locale" and is UTF-8 on
    on Linux.
    BUG: 493180
    FIXED-IN: 4.0.1

--- src/utils/string_utils.cpp
+++ src/utils/string_utils.cpp
@@ -242,7 +242,9 @@ QByteArray Tellico::localeEncodingName() {
 #if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))
   return QTextCodec::codecForLocale()->name();
 #else
-// QStringConverter::nameForEncoding(QStringConverter::System) returns "Locale" which is not what we want
-  return QStringConverter::nameForEncoding(QStringConverter::System);
+// Bug 493180: QStringConverter::nameForEncoding(QStringConverter::System) returns "Locale" which is not what we want
+// return UTF-8 instead
+  const auto encName = QStringConverter::nameForEncoding(QStringConverter::System);
+  return (encName.compare("Locale") == 0) ? "UTF-8" : encName;
 #endif
 }
