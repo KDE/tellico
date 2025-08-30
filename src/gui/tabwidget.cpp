cb172f84c42d5eec46fb3c8b8ba085b841cd384b
Robby Stephenson <robby@periapsis.org>
2025-08-30 15:18:21 -0400

    Connect tab widget next/previous to default shortcuts
    BUG: 508789
    FIXED-IN: 4.1.4

--- src/gui/tabwidget.cpp
+++ src/gui/tabwidget.cpp
@@ -24,12 +24,21 @@
 
 #include "tabwidget.h"
 
+#include <KStandardAction>
+
 #include <QTabBar>
 
 using Tellico::GUI::TabWidget;
 
 TabWidget::TabWidget(QWidget* parent_)
     : QTabWidget(parent_) {
+  auto action = KStandardAction::next(this, &TabWidget::slotNextTab, this);
+  action->setShortcuts(KStandardShortcut::tabNext());
+  addAction(action);
+
+  action = KStandardAction::prior(this, &TabWidget::slotPrevTab, this);
+  action->setShortcuts(KStandardShortcut::tabPrev());
+  addAction(action);
 }
 
 void TabWidget::setFocusToFirstChild() {
@@ -48,4 +57,14 @@ void TabWidget::setTabBarHidden(bool hide_) {
   QTabBar* tabBar = findChild<QTabBar *>();
   Q_ASSERT(tabBar);
   tabBar->setHidden(hide_);
+}
+
+void TabWidget::slotNextTab() {
+  const auto next = currentIndex() + 1;
+  setCurrentIndex(next >= count() ? 0 : next);
+}
+
+void TabWidget::slotPrevTab() {
+  const auto prev = currentIndex() - 1;
+  setCurrentIndex(prev < 0 ? count() - 1 : prev);
 }
